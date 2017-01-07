528bf437f3706393f1f613749228284db733cfe1
daliwangi <daliwangi@gmail.com>
2017-01-07 18:54:53 +0800

    add a new column for relaying.

---
+++ src/CoiniumServ/Persistance/Layers/Hybrid/Migrations/M004AddRelayColumn.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentMigrator;
+
+namespace LGSV.Persistance.Layers.Hybrid.Migrations
+{
+    /// <summary>
+    /// add a column named IsRelayBlock
+    /// </summary>
+    [Migration(20150425)]
+    public class M004AddRelayColumn:Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("Block").AddColumn("IsRelayBlock").AsBoolean().NotNullable().WithDefaultValue("0");
+        }
+
+        public override void Down()
+        {
+            Delete.Column("IsRelayBlock").FromTable("Block");
+        }
+    }
+}

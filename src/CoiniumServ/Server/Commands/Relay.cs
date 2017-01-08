868feedb0dbdc6e1ffda3c712803ffdb857108a9
daliwangi <daliwangi@gmail.com>
2017-01-08 17:25:28 +0800

    Add commands for manually switching relay status at runtime.

---
+++ src/CoiniumServ/Server/Commands/Relay.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoiniumServ.Utils.Commands;
+using CoiniumServ.Relay;
+
+namespace CoiniumServ.Server.Commands
+{
+        [CommandGroup("relay", "Allow you to start share relaying or stop it:start/stop/relayto")]
+        public class RelayCommand : CommandGroup
+        {
+            [Command("start", "Start relaying.")]
+            public void Start(string[] @params)
+            {
+                RelayManager.SetRelay(true);
+            }
+
+            [Command("stop", "end relaying.")]
+            public void Stop(string[] @params)
+            {
+                RelayManager.SetRelay(false);
+            }
+
+            [Command("switchupstream","switch upstream pool")]
+            public void Switchupstream(string[] @params)
+            {
+                RelayManager.ManualSwitchUpstream();
+            }
+        }
+
+}

e321c8f198d6a945f945325804ca1836fbe54a41
daliwangi <daliwangi@gmail.com>
2017-01-08 19:10:27 +0800

    Add user hashrate model for web display.

---
+++ src/CoiniumServ/Server/Web/Models/Pool/WorkerHashrateModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoiniumServ.Server.Web.Models.Pool
+{
+    /// <summary>
+    /// Needs Caution because the data is managed with big volumn.
+    /// </summary>
+    public class WorkerHashrateModel:IDisposable
+    {
+        public Dictionary<string,UInt64> WholeDayData { get; set; }
+
+        public ulong recentMinutes { get; set; }
+
+        public void Dispose()
+        {
+        }
+    }
+}
